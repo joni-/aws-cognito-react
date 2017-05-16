6f9fb9b1c6c2b26b5d62e66782cddc1d9cd809eb
Joni Nevalainen <joni.nevalainen@gofore.com>
2017-05-16 20:24:08 +0300

    update navigation items based whether user is authenticated or not

--- src/Layout.js
+++ src/Layout.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Nav, NavItem } from 'react-bootstrap';
 
-const Navigation = () => (
+const Navigation = ({ isAuthenticated }) => (
   <Nav bsStyle="pills" activeKey={1}>
     <NavItem href="/">Home</NavItem>
-    <NavItem href="/profile">Profile</NavItem>
-    <NavItem href="/login">Log in</NavItem>
-    <NavItem href="/signup">Sign up</NavItem>
+    {isAuthenticated && <NavItem href="/profile">Profile</NavItem>}
+    {!isAuthenticated && <NavItem href="/login">Log in</NavItem>}
+    {!isAuthenticated && <NavItem href="/signup">Sign up</NavItem>}
+    {isAuthenticated && <NavItem href="/logout">Logout</NavItem>}
   </Nav>
 );
 
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.user !== null
+});
+
+const WrappedNavigation = connect(mapStateToProps)(Navigation);
+
 const Layout = ({ children }) => (
   <div>
-    <Navigation />
+    <WrappedNavigation />
     {children}
   </div>
 );
